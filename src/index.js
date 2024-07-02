ac6d72f9ffe5162dc7a12ae6d0635bd3ca5b004a
johanudigital <johan@u-digital.nl>
2024-07-02 14:48:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import SocialPostCheckerTool from './SocialPostCheckerTool';
+import SocialPostCheckerTool from './ImageStyleConverter';
 import './index.css';
 
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
-    <SocialPostCheckerTool />
+    <ImageStyleConverter />
   </Router>,
   document.getElementById('root')
 );
